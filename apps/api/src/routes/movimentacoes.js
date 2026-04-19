445f6e423820dc7d364afe3dc69f9cf681e75fb0
Andrey Schwantes <149903412+andreyssouza@users.noreply.github.com>
2026-04-18 22:54:09 -0300

    Add billing authentication middleware

--- apps/api/src/routes/movimentacoes.js
+++ apps/api/src/routes/movimentacoes.js
@@ -12,6 +12,7 @@ import {
 } from '../services/movimentacoesService.js';
 import { jwtAuthMiddleware } from '../middleware/jwtAuth.js';
 import { requireAtivo } from '../middleware/requireAtivo.js';
+import { ensureLoggedIn, loadAssinante, ensureBillingActive } from '../middleware/authBilling.js';
 import { validate } from '../middleware/validate.js';
 import { auditarAcao } from '../middleware/auditoria.js';
 import { AppError } from '../errors/AppError.js';
